ffc86a0a6bfd3ce4a70b9be9526fb5b4f0700a8c
tushar-chadha <tusharchadha26@gmail.com>
2024-04-10 16:50:45 +0530

    .

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const Color secondaryColor = Color(0xFFFFFFFF);
   static const Color blackColor = Color(0xFF000000);
   static const Color borderColor = Color.fromARGB(255, 219, 219, 219);
+    static const Color TextColor2 = Color.fromARGB(255, 170, 170, 170);
 }
